91420909501aacea0072d32eaf84de742287d5be
iamkiran27 <saikiran.kavali@zemosolabs.com>
2022-03-29 12:42:10 +0530

    configv2

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+const { response } = require("express");
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log("Server started at 5000");
-});
+mongoose
+  .connect(
+    "mongodb+srv://admin:SZU4VZBeqlx7uEaA@cluster0.w3g9c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then((response) => {
+    console.log("Connected to mongodb..");
+    app.listen(5000, () => {
+      console.log("Server started at 5000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
