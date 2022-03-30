97fcf17880d8f5296e8d003781b2e7684cf5d22e
iamkiran27 <saikiran.kavali@zemosolabs.com>
2022-03-30 15:16:29 +0530

    front-login

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import LoginComponent from "./components/authComponents/LoginComponent";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<LoginComponent />} />
+
+        <Route path="/signup" />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
