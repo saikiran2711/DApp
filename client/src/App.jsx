7f7d18a76c14116e917fa7d9b006806333303c54
Sampath Babu <iamsampathbabu@gmail.com>
2022-04-02 16:01:52 +0530

    Semester Details Processing done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import EducationPage from "./components/Dashboard/education";
 import LandingPage from "./components/landingPage";
 import AdminComponent from "./components/authComponents/adminComponent";
 import GeneralDetails from "./components/Dashboard/generalDetails";
+import SemesterComponent from "./components/EducationDetails/SemesterDetails";
 
 function App() {
   return (
